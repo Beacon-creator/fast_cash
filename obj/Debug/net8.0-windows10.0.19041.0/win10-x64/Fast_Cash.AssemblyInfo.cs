470520221a3412e18e16032c89c190c153d8c003
Beacon-creator <adebiyibukola157@gmail.com>
2024-07-16 17:06:32 +0100

    adding appicon

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Fast_Cash.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Fast_Cash.AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("copyright@2024")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("...maximizing cash")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+473f9c3c2da49d0b1374952bc8f5d91d711e745e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e5641b45390c38aec1038908177975b424964761")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fast_Cash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fast_Cash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
